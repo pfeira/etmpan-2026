f2cf7000235203f2b9b20dc8f7154c8c2ef5e1ef
pfeira <31713855+pfeira@users.noreply.github.com>
2025-09-14 23:21:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = 'gestor-torneio-cache-v2.6';
+const CACHE_NAME = 'gestor-torneio-cache-v2.7'; // Versão do cache atualizada
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './etmpan1.jpg',
+  './etmpan1.jpg', // Nome do logo já corrigido aqui também
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js',
   'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Teko:wght@500;600&display=swap'
 ];
 
+// Instala o Service Worker e armazena os arquivos em cache
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -20,6 +21,7 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
+// Intercepta os pedidos e serve os arquivos a partir do cache se disponíveis
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
@@ -33,6 +35,7 @@ self.addEventListener('fetch', event => {
   );
 });
 
+// Limpa caches antigas quando uma nova versão do Service Worker é ativada
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
